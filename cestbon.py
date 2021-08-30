dfd3521496b4b0980c7e1e8b9c0a23ec8d6d6e47
Ashley Savage <chavon1189@gmail.com>
2021-08-30 15:04:14 -0400

    adds order

--- cestbon.py
+++ cestbon.py
@@ -2,8 +2,14 @@ wallet = 100
 
 menu = [["burgers", 10], ["fries", 8], ["onion rings", 8], ["drink", 5]]
 
+print(menu)
+
 class Order:
     def __init__(self, items = [], customer = {}, fulfilled = False):
         self.items = input("What can I get you?")
         self.customer = input("Can I have yur name please?")
         self.fulfilled = fulfilled
+
+    def add_menu_order(self, menu):
+        self.items["order"] = order
+    
