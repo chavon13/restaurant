8f66f9a1f0a7a047061401defb79e9b382a42a68
Ashley Savage <chavon1189@gmail.com>
2021-08-30 20:26:11 -0400

    adds  menu ordering

--- cestbon.py
+++ cestbon.py
@@ -5,14 +5,27 @@ menu = [["burgers", 10], ["fries", 8], ["onion rings", 8], ["drink", 5]]
 print(menu)
 
 class Order:
-    def __init__(self, items = [], customer = {}, fulfilled = False):
-        self.items = input("What can I get you?")
+    def __init__(self, customer = {}, items = [], fulfilled = False):
         self.customer = input("Can I have your name please?")
+        self.items = input("What can I get you?")
         self.fulfilled = fulfilled
-    def enter_order(self):
-        return self.items
+        if self.items == "burgers":
+            print(wallet - 10)
+        elif self.items == "fries":
+            print(wallet - 8)
+        elif self.items == "onion rings":
+            print(wallet - 8)
+        elif self.items ==  "drink":
+            print(wallet - 5)
+        else:
+            print("Please enter your order.")
+    # def enter_order(self):
+    #     return self.items
+
+        
+
 
 
 order1 = Order()
-order1.enter_order
+
     
