d7955d8b0002654b7c6481256e88790824c25d54
Ashley Savage <chavon1189@gmail.com>
2021-08-31 21:35:58 -0400

    adds cx greeting

---
+++ restaurant.py
@@ -0,0 +1,13 @@
+class Restaurant:
+    def __init__(self, menus = {}, customers = {}, orders = []):
+        self.menus = menus
+        self.customers = customers
+        self.orders = orders
+    
+    def greeting(self):
+        new_cx = {}
+        new_cx["name"] = input("Hello there! Can I have your name please?")
+        new_cx["number"] = input("May I please have a phone number for your order?")
+        self.new_customer(new_cx["name"], new_cx["number"])
+
+print(customers)
